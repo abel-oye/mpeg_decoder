cce9c016b4b0e04c606912e39db487cf120650cf
MROS <yc1043@gmail.com>
2016-08-10 23:09:19 +0800

    read sequence header

--- mpeg_decoder.js
+++ mpeg_decoder.js
@@ -2,6 +2,10 @@ class Player {
   constructor(path, canvas) {
       this.path = path;
       this.canvas = canvas;
+
+      // readbits 所需
+      this.bit_counter = 0;
+      this.byte_buf = null;
   }
 
   loadVideo(params) {
@@ -13,9 +17,6 @@ class Player {
       that.buffer = new Uint8Array(arrayBuffer);
       that.pointer = 0;
       console.log(that.buffer.length);
-      for (let i = 0; i < 10; i++) {
-        console.log(that.buffer[i]);
-      }
       if (params.autoplay) {
         console.log("start play");
         that.play();
@@ -29,35 +30,87 @@ class Player {
     return ret;
   }
 
+  readbits(num) {
+    var ret = 0;
+    for (let i = 0; i < num; i++) {
+      if (this.bit_counter == 0) {
+        this.byte_buf = this.readbyte();
+      }
+      ret = (ret << 1) + ((this.byte_buf & (1 << (7 - this.bit_counter))) > 0 ? 1 : 0);
+      this.bit_counter = this.bit_counter == 7 ? 0 : this.bit_counter + 1;
+    }
+    return ret;
+  }
+
   readbytes (num) {
     var ret = this.buffer.slice(this.pointer, this.pointer + num);
     this.pointer += num;
     return ret;
   }
 
-  // 讀取 sequence header ，正確回傳 true
-  read_sequence_header () {
-    var header = this.readbytes(4);
+  read_start_code (match_code) {
+    var code = this.readbytes(match_code.length);
     var ok = true;
-    ok = ok && (header[0] == 0x00);
-    ok = ok && (header[1] == 0x00);
-    ok = ok && (header[2] == 0x01);
-    ok = ok && (header[3] == 0xB3);
+    for (let i = 0; i < match_code.length; i++) {
+      ok = ok && (code[i] == match_code[i]);
+    }
+    ok = ok && (code[0] == match_code[0]);
+    ok = ok && (code[1] == match_code[1]);
+    ok = ok && (code[2] == match_code[2]);
+    ok = ok && (code[3] == match_code[3]);
     return ok;
   }
 
-  play() {
-    if (this.read_sequence_header() == false) {
+  // 讀取 sequence header ，正確回傳 true
+  read_sequence_header () {
+    if (this.read_start_code([0x00, 0x00, 0x01, 0xB3]) == false) {
       console.error("read sequence header error");
     } else {
       console.log("read sequence header success");
     }
-    // 接著讀取前十二 byte 為高度
-    var height = this.readbyte();
-    var mid = this.readbyte();
-    height = height * 16 + (mid >> 4);
-    var width = (mid % 16) * 256 + this.readbyte();
-    console.log("長寬為 " + height + " * " + width);
+    // 接著讀取的 24 bytes 中，前 12 bytes 為寬度，後 12 bytes 為高度
+    var width = this.readbyte();
+    var tmp = this.readbyte();
+    width = width * 16 + (tmp >> 4);
+    var height = (tmp % (1 << 4)) * 256 + this.readbyte();
+    console.log("寬*高為 " + width + " * " + height);
+    this.canvas.width = width;
+    this.canvas.height = height;
+    var read_and_log = function (str, len) {
+      var ret = this.readbits(len);
+      console.log(str + " = " + ret);
+      return ret;
+    }.bind(this);
+    read_and_log("pel_aspect_rate", 4);
+    read_and_log("picture_rate", 4);
+    read_and_log("bit_rate", 18);
+    read_and_log("marker_bit", 1);
+    read_and_log("vbv_buffer_size ", 10);
+    read_and_log("constrained_parameter_flag", 1);
+    var should_load = read_and_log("load_intra_quantizer_matrix ", 1);
+    if (should_load) {
+      this.readbits(8*64);
+      // TODO: 處理量化矩陣
+    }
+    should_load = read_and_log("load_non_intra_quantizer_matrix", 1);
+    if (should_load) {
+      this.readbits(8*64);
+      // TODO: 處理量化矩陣
+    }
+
+    // TODO: 處理 extension_start_code
+  }
+  read_group_of_pictures() {
+    if (this.read_start_code([0x00, 0x00, 0x01, 0xB8]) == false) {
+      console.error("read picture start code error");
+    } else {
+      console.log("read picture start code success");
+    }
+  }
+
+  play() {
+    this.read_sequence_header();
+    this.read_group_of_pictures();
     var ctx = this.canvas.getContext('2d');
     ctx.fillStyle = "rgb(200,0,0)";
     ctx.fillRect (10, 10, 55, 50);
