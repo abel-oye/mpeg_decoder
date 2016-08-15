0c43cd574b5b9d808223227c7cff6ce5bba7c83a
MROS <yc1043@gmail.com>
2016-08-15 18:40:49 +0800

    可由原始vlc表格建立多層表格

--- vlc.js
+++ vlc.js
@@ -23,15 +23,51 @@ function *generate_all_prefix(code, length) {
   }
 }
 
-function createMatcher(info, size_per_level) {
-  var len = size_per_level > info.max_code_length ? size_per_level : info.max_code_length;
-  var table = new Array(1 << len);
-  for (let item of info.table) {
-    let code = item[0];
-    let value = item[1];
-
+function create_match_table(info, size_per_level) {
+  // 先計算有多少個 level
+  var size_in_levels = [];
+  var remain = info.max_code_length;
+  while (true) {
+    if (remain > size_per_level) {
+      size_in_levels.push(size_per_level);
+      remain -= size_per_level;
+    } else {
+      size_in_levels.push(remain)
+      break;
+    }
   }
+  console.log(size_in_levels);
+
+  function insert_table(table, code, value, level) {
+    var size = size_in_levels[level];
+    if (code.length <= size) {
+      for (let i of generate_all_prefix(code, size)) {
+        table[i] = value;
+      }
+    } else {
+      console.log(code.slice(0, size));
+      var index = code_to_num(code.slice(0, size));
+      var next_table = table[index];
+      console.log(next_table);
+      if (next_table == undefined) {
+        next_table = new Array(1 << size_in_levels[level + 1]);
+      }
+      table[index] = next_table;
+      var next_code = code.slice(size, code.length);
+      insert_table(next_table, next_code, value, level + 1);
+    }
+  }
+
+  var table = new Array(1 << size_in_levels[0]);
+  for (let item of info.table) {
+    var code = item[0];
+    var value = item[1];
+    insert_table(table, code, value, 0)
+  }
+  return table;
 }
+
+
 const MACROBLOCK_ADDRESS_INCREMENT_INFO = {
   max_code_length: 11,
   table: [
@@ -88,3 +124,10 @@ const MACROBLOCK_TYPE_I_INFO = {
     [[0, 1], {quant: 1, motion_forward: 0, motion_backward: 0, pattern: 0, intra: 1}],
   ]
 };
+
+var t = create_match_table(MACROBLOCK_TYPE_I_INFO, NORMAL_SIZE_PER_LEVEL)
+console.log(t[0]);
+console.log(t[1]);
+console.log(t[2]);
+console.log(t[3]);
+var t2 = create_match_table(MACROBLOCK_ADDRESS_INCREMENT_INFO, NORMAL_SIZE_PER_LEVEL);
